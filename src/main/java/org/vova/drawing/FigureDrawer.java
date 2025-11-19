c6e76c6f4bf3c2fa9b3ccffcaa67f0eb833139cd
wolfgang495 <vovann.2283224@gmail.com>
2025-11-19 11:25:49 +0100

    Initial commit

---
+++ src/main/java/org/vova/drawing/FigureDrawer.java
@@ -0,0 +1,20 @@
+package org.vova.drawing;
+
+public class FigureDrawer {
+    public static void main(String[] args) {
+        int size = 7;
+        drawHorizontalLine(size);
+    }
+
+    private static void drawHorizontalLine(int size) {
+        // TODO: Implement
+    }
+
+    private static void drawVerticalLine(int size) {
+        // TODO: Implement
+    }
+
+    private static void drawSquare(int size) {
+        // TODO: Implement
+    }
+}
