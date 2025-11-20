ebbc9d0b6c65ff0c7c3b0ce615b9e912aa78dcbc
wolfgang495 <vovann.2283224@gmail.com>
2025-11-20 18:07:24 +0100

    add drawTraingle1 for+for

--- src/main/java/org/vova/drawing/FigureDrawer.java
+++ src/main/java/org/vova/drawing/FigureDrawer.java
@@ -3,11 +3,15 @@ package org.vova.drawing;
 public class FigureDrawer {
     public static void main(String[] args) {
         int size = 7;
-        drawHorizontalLine(size);
-        System.out.println();
-        drawVerticalLine(size);
-        System.out.println();
-        drawSquare(size);
+        //drawHorizontalLine(size);
+        //System.out.println();
+        //drawVerticalLine(size);
+        //System.out.println();
+        //drawSquare(size);
+        //System.out.println();
+        drawTriangle1(size);
+        //System.out.println();
+        //drawTriangle2(size);
     }
 
     //If size >= 1
@@ -37,5 +41,26 @@ public class FigureDrawer {
         if (size != 1) {
             drawHorizontalLine(size);
         }
+    }
+
+
+    private static void drawTriangle1(int size) {
+        int space = 0;
+        System.out.println("*");
+        for (int i = 1; i <= size - 2; i++) {
+            System.out.print("*");
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+            space = space + 1;
+        }
+        if (size != 1) {
+            drawHorizontalLine(size);
+        }
+    }
+
+    private static void drawTriangle2(int size) {
+
     }
 }
