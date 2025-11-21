9acb5d5b0e2a506ce99575aaeb98bf1854a5ebff
wolfgang495 <vovann.2283224@gmail.com>
2025-11-21 19:20:08 +0100

    code refactoring exercise 1

--- src/main/java/org/vova/drawing/FigureDrawer.java
+++ src/main/java/org/vova/drawing/FigureDrawer.java
@@ -3,57 +3,53 @@ package org.vova.drawing;
 public class FigureDrawer {
     public static void main(String[] args) {
         int size = 7;
-        //drawHorizontalLine(size);
-        //System.out.println();
-        //drawVerticalLine(size);
-        //System.out.println();
-        //drawSquare(size);
-        //System.out.println();
-        //drawTriangle1(size);
-        //System.out.println();
+        drawHorizontalLine(size);
+        System.out.println();
+        drawVerticalLine(size);
+        System.out.println();
+        drawSquare(size);
+        System.out.println();
+        drawTriangle1(size);
+        System.out.println();
         drawTriangle2(size);
     }
 
     //If size >= 1
     private static void drawHorizontalLine(int size) {
-        for (int i = 1; i <= size; i++) {
+        for (int i = 0; i < size; i++) {
             System.out.print("*");
         }
         System.out.println();
     }
 
     private static void drawVerticalLine(int size) {
-        for (int i = 1; i <= size; i++) {
+        for (int i = 0; i < size; i++) {
             System.out.println("*");
         }
     }
 
     private static void drawSquare(int size) {
         drawHorizontalLine(size);
-        for (int i = 1; i <= size - 2; i++) {
+        for (int i = 0; i < size - 2; i++) {
             System.out.print("*");
-            for (int j = 1; j <= size - 2; j++) {
+            for (int j = 0; j < size - 2; j++) {
                 System.out.print(" ");
             }
-            System.out.print("*");
-            System.out.println();
+            System.out.println("*");
         }
         if (size != 1) {
             drawHorizontalLine(size);
         }
     }
 
-
     private static void drawTriangle1(int size) {
-        int space = 0;
         System.out.println("*");
-        for (int i = 1; i <= size - 2; i++) {
+        for (int i = 0; i < size - 2; i++) {
             System.out.print("*");
-            for (int j = 1; j <= space; j++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
             System.out.println("*");
-            space = space + 1;
         }
         if (size != 1) {
             drawHorizontalLine(size);
