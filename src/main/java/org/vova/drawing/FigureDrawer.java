aef3134e4a62201e432857b5400c00c76a711688
wolfgang495 <vovann.2283224@gmail.com>
2025-11-22 18:05:58 +0100

    разделение методов на три группы

--- src/main/java/org/vova/drawing/FigureDrawer.java
+++ src/main/java/org/vova/drawing/FigureDrawer.java
@@ -1,10 +1,7 @@
 package org.vova.drawing;
 
 public class FigureDrawer {
-
-    public static final String ASTERIX = "*";
-    public static final String SPACE = " ";
-
+    //Первая группа: главный клиент, который инициирует начальные действия
     public static void main(String[] args) {
         int size = 7;
         drawHorizontalLine(size);
@@ -18,28 +15,29 @@ public class FigureDrawer {
         drawTriangle2(size);
     }
 
+    //Вторая группа: (функциональные) методы, рисующие фигуры полностью
     //If size >= 1
     private static void drawHorizontalLine(int size) {
         for (int i = 0; i < size; i++) {
-            System.out.print(ASTERIX);
+            System.out.print("*");
         }
         System.out.println();
     }
 
     private static void drawVerticalLine(int size) {
         for (int i = 0; i < size; i++) {
-            System.out.println(ASTERIX);
+            System.out.println("*");
         }
     }
 
     private static void drawSquare(int size) {
         drawHorizontalLine(size);
         for (int i = 0; i < size - 2; i++) {
-            System.out.print(ASTERIX);
+            System.out.print("*");
             for (int j = 0; j < size - 2; j++) {
-                System.out.print(SPACE);
+                System.out.print(" ");
             }
-            System.out.println(ASTERIX);
+            System.out.println("*");
         }
         if (size != 1) {
             drawHorizontalLine(size);
@@ -47,13 +45,13 @@ public class FigureDrawer {
     }
 
     private static void drawTriangle1(int size) {
-        System.out.println(ASTERIX);
+        System.out.println("*");
         for (int i = 0; i < size - 2; i++) {
-            System.out.print(ASTERIX);
+            System.out.print("*");
             for (int j = 0; j < i; j++) {
-                System.out.print(SPACE);
+                System.out.print(" ");
             }
-            System.out.println(ASTERIX);
+            System.out.println("*");
         }
         if (size != 1) {
             drawHorizontalLine(size);
@@ -63,23 +61,25 @@ public class FigureDrawer {
     private static void drawTriangle2(int size) {
         int midHeight = size / 2 - 1;
         for (int i = 0; i <= midHeight; i++) {
-            System.out.print(SPACE);
+            System.out.print(" ");
         }
-        System.out.println(ASTERIX);
+        System.out.println("*");
 
         for (int i = 0; i < midHeight; i++) {
             for (int j = 0; j < midHeight - i; j++) {
-                System.out.print(SPACE);
+                System.out.print(" ");
             }
-            System.out.print(ASTERIX);
+            System.out.print("*");
             for (int k = 1; k <= 2 * i + 1; k++) {
-                System.out.print(SPACE);
+                System.out.print(" ");
             }
-            System.out.println(ASTERIX);
+            System.out.println("*");
         }
 
         if (size != 1) {
             drawHorizontalLine(size);
         }
     }
+
+    //Третья группа: (сервисные) методы, которым делигировано рисование отдельных частей фигур
 }
