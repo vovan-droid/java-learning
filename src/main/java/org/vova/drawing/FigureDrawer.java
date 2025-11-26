20dbf4dbdcc4c353b43f140c9e9a1e85bef5c6d7
wolfgang495 <vovann.2283224@gmail.com>
2025-11-26 20:50:43 +0100

    задания до 2.8

--- src/main/java/org/vova/drawing/FigureDrawer.java
+++ src/main/java/org/vova/drawing/FigureDrawer.java
@@ -6,11 +6,11 @@ public class FigureDrawer {
     public static final String SPACE = " ";
 
     //Первая группа: главный клиент, который инициирует начальные действия
+
+
     public static void main(String[] args) {
         int size = 7;
         drawHorizontalLine(size);
-        System.out.println();
-        drawHorizontalLineWithPrintElementWithForLn(size);
         System.out.println();
         drawVerticalLine(size);
         System.out.println();
@@ -24,7 +24,7 @@ public class FigureDrawer {
         System.out.println();
         drawTriangle1WithDottedLine(size);
         System.out.println();
-        drawTriangle1WithPrintElementWithForLn(size);
+        drawTriangle1WithPrintSpacesAndNewLineIfChar1Empty(size);
         System.out.println();
         drawTriangle2(size);
         System.out.println();
@@ -34,30 +34,26 @@ public class FigureDrawer {
     }
 
     //Вторая группа: (функциональные) методы, рисующие фигуры полностью
+
+
     //If size >= 1
+
     private static void drawHorizontalLine(int size) {
         for (int i = 0; i < size; i++) {
             System.out.print(ASTERIX);
         }
         System.out.println();
     }
-
-    private static void drawHorizontalLineWithPrintElementWithForLn(int size) {
-        printElementWithForLn(ASTERIX,"", size, 1);
-    }
-
+    
     private static void drawVerticalLine(int size) {
         for (int i = 0; i < size; i++) {
             System.out.println(ASTERIX);
         }
     }
-// Можно теоретически и метод "drawVerticalLine" сделать используя сервисный метод, если добавить Чаром ASTERIX с переводом строки
-//    private static void drawVerticalLineWithPrintElementWithFor(int size) {
-//        printElementWithForLn(ASTERIXLn, size, 0, 0);
-//    }
 
     private static void drawSquare(int size) {
         drawHorizontalLine(size);
+
         for (int i = 0; i < size - 2; i++) {
             System.out.print(ASTERIX);
             for (int j = 0; j < size - 2; j++) {
@@ -65,6 +61,7 @@ public class FigureDrawer {
             }
             System.out.println(ASTERIX);
         }
+
         if (size != 1) {
             drawHorizontalLine(size);
         }
@@ -72,27 +69,32 @@ public class FigureDrawer {
 
     private static void drawSquareWithDottedLine(int size) {
         drawHorizontalLine(size);
+
         for (int i = 0; i < size - 2; i++) {
-            drawDottedLine(0, size - 2);
+            printDottedLine(0, size - 2);
         }
+
         if (size != 1) {
             drawHorizontalLine(size);
         }
     }
 
     private static void drawSquareWithPrintElementWithForLn(int size) {
-        drawHorizontalLineWithPrintElementWithForLn(size);
+        drawHorizontalLine(size);
+
         for (int i = 0; i < size - 2; i++) {
             System.out.print(ASTERIX);
-            printElementWithForLn(SPACE, ASTERIX, size - 2, 1);
+            printSpacesAndAsterixWithNewLineIfNeeded(size - 2, true);
         }
+
         if (size != 1) {
-            drawHorizontalLineWithPrintElementWithForLn(size);
+            drawHorizontalLine(size);
         }
     }
 
     private static void drawTriangle1(int size) {
         System.out.println(ASTERIX);
+
         for (int i = 0; i < size - 2; i++) {
             System.out.print(ASTERIX);
             for (int j = 0; j < i; j++) {
@@ -100,27 +102,34 @@ public class FigureDrawer {
             }
             System.out.println(ASTERIX);
         }
+
         if (size != 1) {
             drawHorizontalLine(size);
         }
     }
 
     private static void drawTriangle1WithDottedLine(int size) {
-        drawDottedLine(0);
+        printDottedLine(0);
+
         for (int i = 0; i < size - 2; i++) {
-            drawDottedLine(0, i);
+            printDottedLine(0, i);
         }
-        drawHorizontalLine(size);
+
+        if (size != 1) {
+            drawHorizontalLine(size);
+        }
     }
 
-    private static void drawTriangle1WithPrintElementWithForLn(int size) {
+    private static void drawTriangle1WithPrintSpacesAndNewLineIfChar1Empty(int size) {
         System.out.println(ASTERIX);
+
         for (int i = 0; i < size - 2; i++) {
             System.out.print(ASTERIX);
-            printElementWithForLn(SPACE, ASTERIX, i, 1);
+            printSpacesAndAsterixWithNewLineIfNeeded(i, true);
         }
+
         if (size != 1) {
-            drawHorizontalLineWithPrintElementWithForLn(size);
+            drawHorizontalLine(size);
         }
     }
 
@@ -149,10 +158,12 @@ public class FigureDrawer {
 
     private static void drawTriangle2WithDottedLine(int size) {
         int midHeight = size / 2 - 1;
-        drawDottedLine(midHeight + 1);
+        printDottedLine(midHeight + 1);
+
         for (int i = 0; i < midHeight; i++) {
-            drawDottedLine(midHeight - i, 2 * i + 1);
+            printDottedLine(midHeight - i, 2 * i + 1);
         }
+
         if (size != 1) {
             drawHorizontalLine(size);
         }
@@ -160,23 +171,34 @@ public class FigureDrawer {
 
     private static void drawTriangle2WithPrintElementWithForLn(int size) {
         int midHeight = size / 2 - 1;
-        printElementWithForLn(SPACE, ASTERIX, midHeight + 1, 1);
+        printSpacesAndAsterixWithNewLineIfNeeded(midHeight + 1, true);
 
         for (int i = 0; i < midHeight; i++) {
-            printElementWithForLn(SPACE, "", midHeight - i, 0);
+            printSpacesAndAsterixWithNewLineIfNeeded(midHeight - i, false);
             System.out.print(ASTERIX);
-            printElementWithForLn(SPACE, ASTERIX, 2 * i + 1, 1);
+            printSpacesAndAsterixWithNewLineIfNeeded(2 * i + 1, true);
         }
 
         if (size != 1) {
-            drawHorizontalLineWithPrintElementWithForLn(size);
+            drawHorizontalLine(size);
         }
     }
 
     //Третья группа: (сервисные) методы, которым делигировано рисование отдельных частей фигур
 
-    //Делает пробелы до звездочек в цикле и ставит их
-    private static void drawDottedLine(int... amountOfSpacesBeforeAsterix) {
+
+    //Печатает пробелы в цикле и ставит звездочку с переводом строки, если требуется
+    private static void printSpacesAndAsterixWithNewLineIfNeeded(int amountOfSpaces, boolean asterixNewLine) {
+        for (int i = 0; i < amountOfSpaces; i++) {
+            System.out.print(SPACE);
+        }
+        if (asterixNewLine) {
+            System.out.println(ASTERIX);
+        }
+    }
+
+    //Печатает пробелы до звездочек в цикле и ставит их
+    private static void printDottedLine(int... amountOfSpacesBeforeAsterix) {
         for (int i = 0; i < amountOfSpacesBeforeAsterix.length; i++) {
             for (int j = 0; j < amountOfSpacesBeforeAsterix[i]; j++) {
                 System.out.print(SPACE);
@@ -184,15 +206,5 @@ public class FigureDrawer {
             System.out.print(ASTERIX);
         }
         System.out.println();
-    }
-
-    //Делает пробелы в цикле и если нужно использует println()
-    private static void printElementWithForLn(String character, String character1, int... amountOfRepeatsAndElement) {
-        for (int j = 0; j < amountOfRepeatsAndElement[0]; j++) {
-            System.out.print(character);
-        }
-        if (amountOfRepeatsAndElement[1] == 1) {
-            System.out.println(character1);
-        }
     }
 }
