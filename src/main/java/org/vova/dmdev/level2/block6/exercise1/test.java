decf6f9d92e935583cdc6f31d7c25de1a13c35db
wolfgang495 <vovann.2283224@gmail.com>
2025-12-13 20:30:20 +0100

    dmdev: block 6 exercise 1: (сам)

---
+++ src/main/java/org/vova/dmdev/level2/block6/exercise1/test.java
@@ -0,0 +1,9 @@
+package org.vova.dmdev.level2.block6.exercise1;
+
+public class test {
+
+    public static void main(String[] args) {
+        Pair<String, Integer> pair = new Pair<>("key", 20);
+        System.out.println(pair);
+    }
+}
