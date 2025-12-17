253188868b8f1af8a9364299917ffcd1db5a1756
wolfgang495 <vovann.2283224@gmail.com>
2025-12-17 21:58:32 +0100

    dmdev: block 6 exercise 4: (с денисом)

---
+++ src/main/java/org/vova/dmdev/level2/block6/exercise4/Person.java
@@ -0,0 +1,48 @@
+package org.vova.dmdev.level2.block6.exercise4;
+
+import java.util.Objects;
+
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
