d248864cc33bb9d4b1a830d0c1cd2fe57a59f4b8
wolfgang495 <vovann.2283224@gmail.com>
2025-12-22 18:16:54 +0100

    dmdev: block 6 exercise 8 task 1: (сам и c денисом)

---
+++ src/main/java/org/vova/dmdev/level2/block6/exercise8/task1/task1Runner.java
@@ -0,0 +1,23 @@
+package org.vova.dmdev.level2.block6.exercise8.task1;
+
+import java.util.*;
+
+public class task1Runner {
+
+    public static void main(String[] args) {
+        String engText = new String("apple,# Apple Banana cat apple");
+        System.out.println(engText);
+        stringSplitter(engText);
+    }
+
+    public static void stringSplitter(String engText) {
+        String[] words = engText.split("\\W+");
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
