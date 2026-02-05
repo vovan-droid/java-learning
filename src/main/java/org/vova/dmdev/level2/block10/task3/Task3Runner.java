2a003dee2b6bde876895d5c1408fdae57c32aa6b
wolfgang495 <vovann.2283224@gmail.com>
2026-02-05 23:55:20 +0100

    dmdev: block 12 exercise 1: (сам)

---
+++ src/main/java/org/vova/dmdev/level2/block10/task3/Task3Runner.java
@@ -0,0 +1,4 @@
+package org.vova.dmdev.level2.block10.task3;
+
+public class Task3Runner {
+}
---
+++ src/main/java/org/vova/dmdev/level2/block12/task1/task1Runner.java
@@ -0,0 +1,14 @@
+package org.vova.dmdev.level2.block12.task1;
+
+import java.time.LocalDateTime;
+
+public class task1Runner {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.of(2020, 06, 25, 19, 47);
+        System.out.println(localDateTime);
+        LocalDateTime localDateTime2 = localDateTime.plusMonths(3);
+        System.out.println(localDateTime2);
+        System.out.println(localDateTime2.toLocalDate());
+        System.out.println(localDateTime2.toLocalTime());
+    }
+}
