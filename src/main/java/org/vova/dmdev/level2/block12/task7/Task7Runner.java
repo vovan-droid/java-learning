331f310bea80c386f934e1716e0d9358ad3489c0
wolfgang495 <vovann.2283224@gmail.com>
2026-02-06 02:01:37 +0100

    dmdev: block 12 exercise 7: (сам и с денисом)

--- src/main/java/org/vova/dmdev/level2/block12/task7/Task7Runner.java
+++ src/main/java/org/vova/dmdev/level2/block12/task7/Task7Runner.java
@@ -8,7 +8,6 @@ public class Task7Runner {
     public static void main(String[] args) {
         Instant instant = Instant.now();
         System.out.println(instant);
-
         ZonedDateTime zonedDateTime = instant.atZone(ZoneId.of("Africa/Cairo"));
         System.out.println(zonedDateTime);
     }
